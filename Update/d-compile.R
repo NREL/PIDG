a4b4de388e284484c7b35281f357ec31b642fd9c
Ali <ali.ehlen@nrel.gov>
2016-07-06 11:27:48 -0600

    update d-compile

--- Update/d-compile.R
+++ Update/d-compile.R
@@ -1,7 +1,12 @@
 # setup
-pacman::p_load(data.table)
+pacman::p_load(data.table, openxlsx)
 
-inputfiles.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/InputFiles_tester/"
+inputfiles.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/InputFiles_tester"
+india.repo = TRUE
+
+outputfiles.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/OutputFiles"
+output.wb.name <- "ae_7_6_test_new_framework"
+
 
 
 # variables
@@ -16,7 +21,7 @@ inputfiles.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/InputFiles_teste
 # inputfiles
 source("~/GitHub/India_GtG/Process_for_PLEXOS_import/input_params_cea_wo_status0_or_stranded_node_lines_tester.R")
 
-
+choose.db <- "cea"
 
 # call
 root.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/PSSE2PLEXOS/Update/"
