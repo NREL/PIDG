eb3bb18fe213c3a1ceac7dd9a3d8fd67097aba9d
Ali <ali.ehlen@nrel.gov>
2016-07-06 00:09:18 -0600

    update driver script

---
+++ Update/d-compile.R
@@ -0,0 +1,29 @@
+# setup
+pacman::p_load(data.table)
+
+root.dir <- "~/GitHub/India_GtG/Process_for_PLEXOS_import/PSSE2PLEXOS/Update/"
+
+
+
+# variables
+
+node.file <- file.path(root.dir, "outputs_a-3_corrected_psse/node.data.csv")
+line.file <- file.path(root.dir, "outputs_a-3_corrected_psse/line.data.csv")
+generator.file <- file.path(root.dir, "outputs_a-3_corrected_psse/generator.data.csv")
+
+transformer.file <- file.path(root.dir, "outputs_a-3_corrected_psse/transformer.data.csv")
+
+load.file <- file.path(root.dir, "outputs_a-3_corrected_psse/load.data.csv")
+
+
+
+
+
+# call
+source(file.path(root.dir, "compile/functions.R"))
+source(file.path(root.dir, "compile/b_create_sheet_tables.R"))
+source(file.path(root.dir, "compile/c1_populate_sheet_tables_with_raw_tables.R"))
+source(file.path(root.dir, "compile/c2_more_data_population.R"))
+source(file.path(root.dir, "compile/c3_create_scenarios_and_models.R"))
+source(file.path(root.dir, "compile/d_data_cleanup.R"))
+source(file.path(root.dir, "compile/e_export_to_excel.R"))
