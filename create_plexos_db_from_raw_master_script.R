ea37367c8f07a7829f6af2d26265ef435ce1432d
Ali <ali.ehlen@nrel.gov>
2016-08-11 17:11:35 -0600

    update master_script to point to new files

--- create_plexos_db_from_raw_master_script.R
+++ create_plexos_db_from_raw_master_script.R
@@ -18,16 +18,16 @@ india.repo = any(strsplit(getwd(),split="/")[[1]]=='India_GtG')
 
 #install.packages("pacman")
 pacman::p_load(cowplot, plyr, dplyr, ggplot2, grid, gridExtra, gtools, 
-  knitr, lubridate, reshape2, data.table, RSQLite, stringr, psych, igraph) 
+               knitr, lubridate, reshape2, data.table, RSQLite, stringr, psych, igraph) 
 pacman::p_load(openxlsx)  
-  # had to follow instructions here: https://github.com/awalker89/openxlsx, 
-  # including installing Rtools from here: 
-  # https://cran.r-project.org/bin/windows/Rtools/
-  # and making sure PATH variable was edited appropriately 
-  # (check the "edit path" box during installation)
+# had to follow instructions here: https://github.com/awalker89/openxlsx, 
+# including installing Rtools from here: 
+# https://cran.r-project.org/bin/windows/Rtools/
+# and making sure PATH variable was edited appropriately 
+# (check the "edit path" box during installation)
 
 
-source(file.path(master.script.dir, "Update/compile/functions.R"))
+source(file.path(master.script.dir, "SourceScripts/functions.R"))
 
 
 #------------------------------------------------------------------------------|
@@ -43,91 +43,53 @@ source(input.params.location)
 # function definition ----
 #------------------------------------------------------------------------------|
 
-if (choose.input == 'raw.psse') {
-    runAllFiles <- function () {
-      message("importing PSSE files...")
-      source(file.path(master.script.dir,
-        "Update/compile/a-1-parse-psse.R"))
-      source(file.path(master.script.dir,
-        "Update/compile/a-2-reformat-psse.R"))
-      message("creating tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/b_create_sheet_tables.R"))
-      message("populating tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/c1_populate_sheet_tables_with_raw_tables.R"))
-      source(file.path(master.script.dir,
-        "Update/compile/c2_more_data_population.R"))
-      source(file.path(master.script.dir,
-        "Update/compile/c3_create_scenarios_and_models.R"))
-      message("cleaning tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/d_data_cleanup.R"))
-      if(data.checks.and.plots == T){
-        message("checking data and creating summary plots...")
-        source(file.path(master.script.dir,
-          "Update/compile/e_summarize_and_check_compiled_database.R"))
-      }
-      message("exporting tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/f_export_to_excel.R"))
-    }
-} else if (choose.input == 'pre.parsed') {
-    runAllFiles <- function () {
-      # message("importing PSSE files...")
-      # source(file.path(master.script.dir,
-      #   "Update/compile/a_import_raw.R"))
-      message("creating tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/b_create_sheet_tables.R"))
-      message("populating tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/c1_populate_sheet_tables_with_raw_tables.R"))
-      source(file.path(master.script.dir,
-        "Update/compile/c2_more_data_population.R"))
-      source(file.path(master.script.dir,
-        "Update/compile/c3_create_scenarios_and_models.R"))
-      message("cleaning tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/d_data_cleanup.R"))
-      if(data.checks.and.plots == T){
-        message("checking data and creating summary plots")
-        source(file.path(master.script.dir,
-          "Update/compile/e_summarize_and_check_compiled_database.R"))
-      }
-      message("exporting tables...")
-      source(file.path(master.script.dir,
-        "Update/compile/f_export_to_excel.R"))
-    }
-} else {
+if (!(choose.input %in% c("raw.psse", "pre.parsed"))) {    
     
-    stop("Please set 'choose.input' in input_params to 'raw.psse' or 'pre.parsed'")
+    stop(paste("Please set 'choose.input' in input_params",
+               "to 'raw.psse' or 'pre.parsed'"))
 }
+
+
+runAllFiles <- function () {
     
+    # only parse psse if need to
+    if (choose.input == 'raw.psse') {
+        message("importing PSSE files...")
+        source(file.path(master.script.dir, "SourceScripts",
+                         "a-1-parse-psse.R"))
+        source(file.path(master.script.dir, "SourceScripts",
+                         "a-2-reformat-psse.R"))
+    }
     
+    # proceed with rest of data compilation
+    message("creating tables...")
+    source(file.path(master.script.dir, "SourceScripts",
+                     "b_create_sheet_tables.R"))
     
+    message("populating tables...")
+    source(file.path(master.script.dir, "SourceScripts",
+                     "c1_populate_sheet_tables_with_raw_tables.R"))
+    source(file.path(master.script.dir, "SourceScripts",
+                     "c2_more_data_population.R"))
+    source(file.path(master.script.dir, "SourceScripts",
+                     "c3_create_scenarios_and_models.R"))
     
-# runAllFiles <- function () {
-#   message("importing PSSE files...")
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/a_import_raw.R"))
-#   message("creating tables...")
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/b_create_sheet_tables.R"))
-#   message("populating tables...")
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/c1_populate_sheet_tables_with_raw_tables.R"))
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/c2_more_data_population.R"))
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/c3_create_scenarios_and_models.R"))
-#   message("cleaning tables...")
-#   source(file.path(master.script.dir, 
-#     "SourceScripts/d_data_cleanup.R"))
-#   message("exporting tables...")
-#   source(file.path(master.script.dir,
-#     "SourceScripts/e_export_to_excel.R"))
-# }
+    message("cleaning tables...")
+    source(file.path(master.script.dir, "SourceScripts",
+                     "d_data_cleanup.R"))
+    
+    # check data if need to 
+    if(data.checks.and.plots == TRUE){
+        message("checking data and creating summary plots")
+        source(file.path(master.script.dir, "SourceScripts",
+                         "e_summarize_and_check_compiled_database.R"))
+    }
+    
+    # export tables
+    message("exporting tables...")
+    source(file.path(master.script.dir, "SourceScripts",
+                     "f_export_to_excel.R"))
+}
 
 
 #------------------------------------------------------------------------------|
