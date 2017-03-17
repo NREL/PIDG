778f96eed75b581d35d546839f5ec5598e8b637a
Ehlen <aehlen@nrel.gov>
2017-03-17 12:52:09 -0600

    minor reformats of script b

--- SourceScripts/b_create_sheet_tables.R
+++ SourceScripts/b_create_sheet_tables.R
@@ -6,31 +6,56 @@
 
 #create empty character tables, one for each worksheet, for PLEXOS's 
 #required Excel document format
-Objects.sheet <- data.table(class = character(), name = character(), 
-  category = character(), description =  character() )
+Objects.sheet <- data.table(class = character(), 
+                            name = character(), 
+                            category = character(), 
+                            description =  character())
 
-Categories.sheet <- data.table(category_id = character(), class = character(), 
-  category = character(), rank = character(), class_id = character(), 
-  name = character() )
+Categories.sheet <- data.table(category_id = character(), 
+                               class = character(), 
+                               category = character(), 
+                               rank = character(), 
+                               class_id = character(), 
+                               name = character())
 
 Memberships.sheet <- data.table(parent_class = character(), 
-  child_class = character(), collection = character(), 
-  parent_object = character(), child_object = character() )
+                                child_class = character(), 
+                                collection = character(), 
+                                parent_object = character(), 
+                                child_object = character())
 
-Attributes.sheet <- data.table(name = character(), class = character(), 
-  attribute = character(), value = character() )
+Attributes.sheet <- data.table(name = character(), 
+                               class = character(), 
+                               attribute = character(), 
+                               value = character())
 
 Properties.sheet <- data.table(parent_class = character(),	
-  child_class = character(),	collection = character(), 
-  parent_object = character(),	child_object = character(), 
-  property = character(), band_id = character(),	value = character(),	
-  units = character(),	date_from = character(), date_to = character(),	
-  pattern = character(), escalator = character(),	condition = character(),	
-  variable = character(),	filename = character(),	scenario = character(), 
-  memo = character(),	period_type_id = character() )
+                               child_class = character(),	
+                               collection = character(), 
+                               parent_object = character(),	
+                               child_object = character(), 
+                               property = character(), 
+                               band_id = character(),	
+                               value = character(),	
+                               units = character(),	
+                               date_from = character(), 
+                               date_to = character(),	
+                               pattern = character(), 
+                               escalator = character(),	
+                               condition = character(),	
+                               variable = character(),	
+                               filename = character(),	
+                               scenario = character(), 
+                               memo = character(),	
+                               period_type_id = character())
 
-Reports.sheet <- data.table(object = character(), parent_class = character(), 
-  child_class = character(), collection = character(), property = character(), 
-  phase_id = character(), report_period = character(), 
-  report_summary = character(), report_statistics = character(), 
-  report_samples = character() )
+Reports.sheet <- data.table(object = character(), 
+                            parent_class = character(), 
+                            child_class = character(), 
+                            collection = character(), 
+                            property = character(), 
+                            phase_id = character(), 
+                            report_period = character(), 
+                            report_summary = character(), 
+                            report_statistics = character(), 
+                            report_samples = character())
