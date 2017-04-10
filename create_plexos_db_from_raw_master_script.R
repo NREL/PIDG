ef3d79eec4c003c70a855c54f580726eefef5db3
aliehlen1 <ali.ehlen@nrel.gov>
2017-04-10 15:23:31 -0600

    pare down imported packages

--- create_plexos_db_from_raw_master_script.R
+++ create_plexos_db_from_raw_master_script.R
@@ -10,22 +10,49 @@ inputfiles.dir <- args[3]
 outputfiles.dir <- args[4]
 
 
+#------------------------------------------------------------------------------|
+# fill in and check inputs ----
+#------------------------------------------------------------------------------|
+
+# set default choose.input to 'raw.psse' if not set
+if(!exists("choose.input")){
+    choose.input <- "raw.psse"
+}else{
+    if (!(choose.input %in% c("raw.psse", "pre.parsed"))) {    
+        
+        stop(paste("Please set 'choose.input' in input_params",
+                   "to 'raw.psse' or 'pre.parsed'"))
+    }
+}
+
+if (!exists("export.wb")) export.wb <- TRUE
+
+#------------------------------------------------------------------------------|
+# get needed packages and functions ----
+#------------------------------------------------------------------------------|
+
 #******************* CHECKING for India_GtG... 
 # TODO: remove stuff in PSSE2PLEXOS that is India specific
 # but for now, just skip it
 india.repo = any(strsplit(getwd(),split="/")[[1]]=='India_GtG')
 
 
-#install.packages("pacman")
-pacman::p_load(cowplot, plyr, dplyr, ggplot2, grid, gridExtra, gtools, 
-               knitr, lubridate, reshape2, data.table, RSQLite, stringr, psych, igraph) 
-pacman::p_load(openxlsx)  
-# had to follow instructions here: https://github.com/awalker89/openxlsx, 
-# including installing Rtools from here: 
-# https://cran.r-project.org/bin/windows/Rtools/
-# and making sure PATH variable was edited appropriately 
-# (check the "edit path" box during installation)
+if (!("pacman" %in% installed.packages()[, "Package"])) {
+    install.packages("pacman")
+}
 
+if (!("openxlsx" %in% installed.packages()[, "Package"]) & export.wb == TRUE) {
+    stop(paste0("export.wb is set to TRUE but openxlsx is not installed.", 
+                " either set export.wb to FALSE or, to export an excel file,", 
+                " please install openxlsx. follow",
+                " instructions here: https://github.com/awalker89/openxlsx", 
+                " including installing Rtools from here:",  
+                " https://cran.r-project.org/bin/windows/Rtools/", 
+                " and making sure PATH variable was edited",  
+                " (check the 'edit path' box during installation)"))
+}
+
+pacman::p_load(cowplot, ggplot2, data.table, igraph, openxlsx) 
 
 source(file.path(master.script.dir, "SourceScripts/functions.R"))
 
@@ -42,19 +69,6 @@ source(input.params.location)
 #------------------------------------------------------------------------------|
 # function definition ----
 #------------------------------------------------------------------------------|
-
-# set default choose.input to 'raw.psse' if not set
-if(!exists("choose.input")){
-  choose.input <- "raw.psse"
-}else{
-  if (!(choose.input %in% c("raw.psse", "pre.parsed"))) {    
-    
-    stop(paste("Please set 'choose.input' in input_params",
-               "to 'raw.psse' or 'pre.parsed'"))
-  }
-}
-
-if (!exists("export.wb")) export.wb <- TRUE
 
 runAllFiles <- function () {
     
@@ -90,7 +104,7 @@ runAllFiles <- function () {
     if(data.check.plots == TRUE){
         message("checking data and creating summary plots...")
     }else{
-      message("checking data...")
+        message("checking data...")
     }
     source(file.path(master.script.dir, "SourceScripts",
                      "e_summarize_and_check_compiled_database.R"))
@@ -104,7 +118,7 @@ runAllFiles <- function () {
     } else {
         message("export.wb set to false. skipping export.")
     }
-        
+    
 }
 
 
