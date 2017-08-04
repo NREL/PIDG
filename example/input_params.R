2e610caae41bc3366d176f43aad7749fe13530fd
aliehlen1 <ali.ehlen@nrel.gov>
2017-08-04 15:27:59 -0600

    start adding example inputs

---
+++ example/input_params.R
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------|
+# example parameters for test model
+#------------------------------------------------------------------------------|
+
+# optional
+plexos.version <- 7
+
+# import objects with their categories, memberships, and properties
+objects.list <- list(
+    "nodes.csv",
+    "regions.csv",
+    "lines.csv",
+    "generators.csv"
+)
